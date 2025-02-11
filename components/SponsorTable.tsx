98201e50a9963578e18c1c0ca772979f5581fbf2
Patrick Raedler <code@readpato.dev>
2025-02-11 20:27:20 +0100

    feat(SponsorsTable): add Comune Firenze

--- components/SponsorTable.tsx
+++ components/SponsorTable.tsx
@@ -73,6 +73,16 @@ const supporterSponsor: Sponsor[] = [
   }
 ]
 
+const extraSponsors: Sponsor[] = [
+  {
+    alt: "Comune Firenze",
+    href: "https://www.comune.fi.it/",
+    src: "/patrocinio_firenze.png",
+    width: 100,
+    height: 100 
+  }
+]
+
 interface SponsorTier {
   name: string;
   sponsors: Sponsor[];
@@ -94,6 +104,10 @@ const sponsorTiers: SponsorTier[] = [
   {
     name: "Community",
     sponsors: communitySponsors
+  },
+  {
+      name: "Patronage",
+      sponsors: extraSponsors
   }
 ];
 
